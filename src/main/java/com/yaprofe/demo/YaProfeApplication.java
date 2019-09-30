b430c289e5a2f126996a3e88f333052ece1311ae
Luis Esteban Andahur Figueroa <luis.andahur1501@alumnos.ubiobio.cl>
2019-09-30 17:45:24 -0300

    first commit

---
+++ src/main/java/com/yaprofe/demo/YaProfeApplication.java
@@ -0,0 +1,13 @@
+package com.yaprofe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaProfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaProfeApplication.class, args);
+	}
+
+}
